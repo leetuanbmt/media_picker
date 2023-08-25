6b34df7d9df7a4b787565df82e2634b5c73033ce
TuanMinhVan <leetuanbmt@gmail.com>
2023-08-25 08:11:55 +0700

    add flutter hooks

--- lib/core/config.dart
+++ lib/core/config.dart
@@ -9,6 +9,7 @@ export 'package:auto_route/auto_route.dart';
 export 'package:auto_size_text/auto_size_text.dart';
 export 'package:easy_localization/easy_localization.dart' hide TextDirection;
 export 'package:flutter/material.dart';
+export 'package:flutter_hooks/flutter_hooks.dart';
 export 'package:flutter_screenutil/flutter_screenutil.dart';
 export 'package:hooks_riverpod/hooks_riverpod.dart';
 export 'package:velocity_x/velocity_x.dart';
