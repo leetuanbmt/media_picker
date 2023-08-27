f20093354715a5f894eb87fee58e389a97b39d03
VThanh13 <nguyenvanthanh01032001@gmail.com>
2023-08-27 16:45:29 +0700

    merge from develop

--- lib/screens/tabs/notification_tab.dart
+++ lib/screens/tabs/notification_tab.dart
@@ -1,5 +1,4 @@
 import '../../core/config.dart';
-import '../../routes/app_routes.gr.dart';
 
 @RoutePage()
 class NotificationScreen extends StatelessWidget {
@@ -13,17 +12,11 @@ class NotificationScreen extends StatelessWidget {
         title: const Text('Notification'),
       ),
       body: Center(
-        // child: Text(
-        //   'Notification Screen',
-        //   style: TextStyle(
-        //     color: AppTheme.primaryColor,
-        //   ),
-        // ),
-        child: TextButton(
-          child: const Text('abc'),
-          onPressed: () {
-            AutoRouter.of(context).push(const LoginRoute());
-          },
+        child: Text(
+          'Notification Screen',
+          style: TextStyle(
+            color: AppTheme.primaryColor,
+          ),
         ),
       ),
     );
