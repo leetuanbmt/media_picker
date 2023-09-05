c6bc259518f629869e3a593f85b3495acab7beac
TuanMinhVan <leetuanbmt@gmail.com>
2023-09-05 11:17:49 +0700

    remove global key navigator

--- lib/core/utilities/navigator.dart
+++ lib/core/utilities/navigator.dart
@@ -8,19 +8,9 @@ class AppNavigator {
 
   static final AppNavigator instance = AppNavigator._internal();
 
-  // Navigator key for navigation
-  final navigatorKey = GlobalKey<NavigatorState>();
-
   // App router
-  late final appRouter = AppRouter(navigatorKey);
-
-  // App navigator current context
-  BuildContext get currentContext => navigatorKey.currentContext!;
+  late final appRouter = AppRouter();
 
   // App navigator current route
   RouteData get currentRouter => appRouter.topRoute.router.current;
-
-  void push(Widget child) => currentContext.nextPage(child);
-
-  void pop() => navigatorKey.currentState?.maybePop();
 }
--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,14 +1,9 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:flutter/material.dart';
 
 import 'app_routes.gr.dart';
 
 @AutoRouterConfig()
 class AppRouter extends $AppRouter {
-  AppRouter(this.navigatorKey);
-
-  @override
-  final GlobalKey<NavigatorState> navigatorKey;
   @override
   final List<AutoRoute> routes = [
     AutoRoute(
