078399205ffeaf579f2f430f6ee0da22b355a918
TuanMinhVan <leetuanbmt@gmail.com>
2023-10-31 08:53:32 +0700

    fix UI review my asset

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:gmo_media_picker/media_picker.dart';
 
 void main() {
@@ -6,22 +7,43 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primaryColor: const Color(0xff47C3BE),
+          colorScheme: const ColorScheme.light(
+            primary: Color(0xff47C3BE),
+            surface: Color(0xffF5F8FA),
+          ),
+          brightness: Brightness.light,
+          scaffoldBackgroundColor: Colors.white,
+          dividerColor: const Color(0x1F000000),
+          appBarTheme: const AppBarTheme(
+            scrolledUnderElevation: 0,
+            backgroundColor: Color(0xff47C3BE),
+            centerTitle: true,
+            systemOverlayStyle: SystemUiOverlayStyle(
+              statusBarIconBrightness: Brightness.light,
+              statusBarBrightness: Brightness.dark,
+            ),
+            iconTheme: IconThemeData(color: Colors.white),
+            titleTextStyle: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 22,
+              color: Colors.white,
+            ),
+          )),
       home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({super.key});
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
